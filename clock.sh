c4a00050c3bb5d3a60f0d20490e8791454f93239
d6imde9 <50021127+d6imde9@users.noreply.github.com>
2020-11-21 21:18:17 +0800

    Update clock.sh

--- clock.sh
+++ clock.sh
@@ -2,14 +2,14 @@
 uid=201884160115
 upw=09292517
 #使用学号和密码获取ptopid和sid
-curl -1 -d "uid=$uid&upw=$upw&smbtn=健康状况上报平台&hh28=722" "https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/login" -o "udata.txt"
+curl -d "uid=$uid&upw=$upw&smbtn=健康状况上报平台&hh28=722" "https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/login" -o "udata.txt"
 udata='cat udata.txt'
 udata=${udata#*ptopid=}
 udata=${udata%\"*}
 ptopid="${udata%&*}" 
 sid="${udata#*&}"
 #进入身份确认界面
-curl -1 -d "did=1&men6=a&ptopid=$ptopid&sid=$sid" "https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb"
+curl -d "did=1&men6=a&ptopid=$ptopid&sid=$sid" "https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb"
 #进行打卡
 url="https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb?ptopid=${ptopid}&sid=${sid}"
-curl -1 -d "ptopid=$ptopid&sid=$sid" url
+curl -d "ptopid=$ptopid&sid=$sid" url
