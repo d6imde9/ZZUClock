6653ac364b2d568c9e3daaac9b30231db1aaa9b1
d6imde9 <50021127+d6imde9@users.noreply.github.com>
2020-11-22 09:58:20 +0800

    Update clock.sh

--- clock.sh
+++ clock.sh
@@ -2,7 +2,7 @@
 uid=201884160115
 upw=09292517
 #使用学号和密码获取ptopid和sid
-curl -d "uid=$uid&upw=$upw&smbtn=健康状况上报平台&hh28=722" "https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/login" -o "udata.txt"
+curl -d "uid=$uid&upw=$upw&smbtn=健康状况上报平台&hh28=722" "https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/first0" -o "udata.txt"
 udata='cat udata.txt'
 udata=${udata#*ptopid=}
 udata=${udata%\"*}
