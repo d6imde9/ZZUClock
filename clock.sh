a81809b2e42e038e0d13e646c34636fccc6a74db
d6imde9 <50021127+d6imde9@users.noreply.github.com>
2020-11-24 10:04:50 +0800

    Update clock.sh

--- clock.sh
+++ clock.sh
@@ -12,6 +12,7 @@ sid="${udata#*&sid=}"
 url="https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb"
 
 #进入身份确认界面
-curl -d "day6=b&did=1&men6=a&ptopid=$ptopid&sid=$sid" url
+curl -d "day6=b&did=1&men6=a&ptopid=$ptopid&sid=$sid" ${url}
+
 #进行打卡
-curl -d "myvs_1=否&myvs_2=否&myvs_3=否&myvs_4=否&Btn3=获取地市&myvs_13a=41&myvs_13b=4101&myvs_13c=河南省.郑州市.金水区&myvs_14=否&did=2&day6=b&men6=a&ptopid=$ptopid&sid=$sid" url
+curl -d "myvs_1=否&myvs_2=否&myvs_3=否&myvs_4=否&Btn3=获取地市&myvs_13a=41&myvs_13b=4101&myvs_13c=河南省.郑州市.金水区&myvs_14=否&did=2&day6=b&men6=a&ptopid=$ptopid&sid=$sid" ${url}
