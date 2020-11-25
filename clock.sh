473aa3a4b3593c4027aef09027da49bf40face22
d6imde9 <50021127+d6imde9@users.noreply.github.com>
2020-11-25 08:40:31 +0800

    Update clock.sh

--- clock.sh
+++ clock.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-uid="201884160115"
-upw="09292517"
+uid=$1
+upw=$2
 
 #获取ptopid和sid
 curl -d "uid=$uid&upw=$upw&smbtn=健康状况上报平台&hh28=722" "https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/login" -o "udata.txt"
 udata=$(sed -n '11p' udata.txt)
 udata=${udata#*ptopid=}
 udata=${udata%\"\}\}*}
-ptopid="${udata%&*}" 
+ptopid="${udata%&*}"
 sid="${udata#*&sid=}"
 url="https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb"
 
 #进入身份确认界面
-curl -d "day6=b&did=1&men6=a&ptopid=$ptopid&sid=$sid" ${url}
-
+curl -d "day6=b&did=1&men6=a&ptopid=${ptopid}&sid=${sid}" ${url}
 #进行打卡
-curl -d "myvs_1=否&myvs_2=否&myvs_3=否&myvs_4=否&Btn3=获取地市&myvs_13a=41&myvs_13b=4101&myvs_13c=河南省.郑州市.金水区&myvs_14=否&did=2&day6=b&men6=a&ptopid=$ptopid&sid=$sid" ${url}
+curl -d "myvs_1=否&myvs_2=否&myvs_3=否&myvs_4=否&Btn3=获取地市&myvs_13a=41&myvs_13b=4101&myvs_13c=河南省.郑州市.金水区&myvs_14=否&did=2&day6=b&men6=a&ptopid=${ptopid}&sid={$sid}" ${url}
