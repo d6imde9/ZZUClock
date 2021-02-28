c0b5e91236e606c08eb73c90333f50c5cbef2705
d6imde9 <50021127+d6imde9@users.noreply.github.com>
2021-02-28 18:35:56 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -16,5 +16,5 @@ ptopid="${udata%&*}"
 sid="${udata#*&sid=}" #登录获取ptopid和sid
 
 curl -d "day6=b&did=1&men6=a" -d "ptopid=${ptopid}&sid=${sid}" -s ${url2} #进入确认界面
-curl -d "@myvs.txt" -d "jingdu=113.64&weidu=34.71&ptopid=${ptopid}&sid={$sid}" ${url2} #打卡
+curl -d "@myvs.txt" -d "jingdu=113.64&weidu=34.71&ptopid=${ptopid}&sid={$sid}" -s ${url2} #打卡
 done
