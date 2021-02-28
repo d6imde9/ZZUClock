b0311ac0039ed4fdcdd8c0bbcc3e39bebf90e37d
d6imde9 <50021127+d6imde9@users.noreply.github.com>
2021-02-28 18:32:11 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
-
 uid=$1
 upw=$2
-smbtn=进入健康状况上报平台
+IFS=' '
+read -ra uidarr <<< "${uid}" #字符串预处理
+read -ra upwstr <<< "${sid}"
+smbtn="进入健康状况上报平台"
+url1="https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/login"
+url2="https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb"
 
-#获取ptopid和sid
-curl -d "uid=${uid}&upw=${upw}&smbtn=${smbtn}&hh28=722" "https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/login" -o temp.txt
+for(i=0;i<${#uidarr[*]};i++)
+do
+curl -d "uid=${uidarr[i]}&upw=${upwarr[i]}&smbtn=${smbtn}&hh28=722" -s ${url1} -o temp.txt
 udata=$(sed -n '11p' temp.txt)
 udata=${udata#*ptopid=}
 udata=${udata%\"\}\}*}
 ptopid="${udata%&*}"
-sid="${udata#*&sid=}"
+sid="${udata#*&sid=}" #登录获取ptopid和sid
 
-url="https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb"
-#进入身份确认界面
-curl -d "day6=b&did=1&men6=a" -d "ptopid=${ptopid}&sid=${sid}" ${url}
-#进行打卡
-curl -d "@myvs.txt" -d "jingdu=113.64&weidu=34.71&ptopid=${ptopid}&sid={$sid}" ${url}
+curl -d "day6=b&did=1&men6=a" -d "ptopid=${ptopid}&sid=${sid}" -s ${url2} #进入确认界面
+curl -d "@myvs.txt" -d "jingdu=113.64&weidu=34.71&ptopid=${ptopid}&sid={$sid}" ${url2} #打卡
+done
