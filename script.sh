295e3c6e99cf0d29c713be5671273d8b2faf0f22
d6imde9 <50021127+d6imde9@users.noreply.github.com>
2021-02-28 19:30:08 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,11 +3,12 @@ uid=$1
 upw=$2
 uidarr=(${uid//,/}) #字符串预处理
 upwarr=(${upw//,/})
+number=${#uidarr[@]}
 smbtn="进入健康状况上报平台"
 url1="https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/login"
 url2="https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb"
 
-for((i=0;i<${#uidarr[@]};i++))
+for((i=0;i<number;i++))
 do
 curl -d "uid=${uidarr[i]}&upw=${upwarr[i]}&smbtn=$smbtn&hh28=722" -s $url1 -o temp.txt
 udata=$(sed -n '11p' temp.txt)
@@ -15,7 +16,6 @@ udata=${udata#*ptopid=}
 udata=${udata%\"\}\}*}
 ptopid="${udata%&*}"
 sid="${udata#*&sid=}" #登录获取ptopid和sid
-
 curl -d "day6=b&did=1&men6=a" -d "ptopid=$ptopid&sid=$sid" -s $url2 #进入确认界面
 curl -d "@myvs.txt" -d "jingdu=113.64&weidu=34.71&ptopid=$ptopid&sid=$sid" $url2 #打卡
 done
