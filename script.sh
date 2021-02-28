ce1c3f2260b7c5e6ebebe222272e35e0d4497114
d6imde9 <50021127+d6imde9@users.noreply.github.com>
2021-02-28 18:34:04 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-uid=$1
-upw=$2
 IFS=' '
-read -ra uidarr <<< "${uid}" #字符串预处理
-read -ra upwstr <<< "${sid}"
+read -ra uidarr <<< $1 #字符串预处理
+read -ra upwstr <<< $2
 smbtn="进入健康状况上报平台"
 url1="https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/login"
 url2="https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb"
 
-for(i=0;i<${#uidarr[*]};i++)
+for((i=0;i<${#uidarr[*]};i++))
 do
 curl -d "uid=${uidarr[i]}&upw=${upwarr[i]}&smbtn=${smbtn}&hh28=722" -s ${url1} -o temp.txt
 udata=$(sed -n '11p' temp.txt)
