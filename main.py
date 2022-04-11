6573c526c07cc3f90085440494dd00f41fe1592e
d6imde9 <50021127+d6imde9@users.noreply.github.com>
2022-04-11 10:44:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ option.add_argument("blink-settings=imagesEnabled=false")
 option.add_argument('--disable-dev-shm-usage')
 option.add_argument('--ignore-certificate-errors')
 option.add_argument("--disable-extensions")
-prefs = {"profile.default_content_setting_values.geolocation" :2}
-options.add_experimental_option("prefs",prefs)
+pref = {"profile.default_content_setting_values.geolocation" :2}
+option.add_experimental_option("prefs",pref)
 driver = webdriver.Chrome(options=option, executable_path=chromedriver) #启动浏览器
 
 account = os.environ.get('ACCOUNT').split(';') #字符串预处理
@@ -44,7 +44,7 @@ for acc in account:
             res = driver.find_element(by=By.XPATH, value='//*[@id="bak_0"]/div[2]/div[2]/div[2]/div[2]').text
             if "同学，感谢你今日上报健康状况" not in res:
                 errors += 1
-            print(res, '\n')
+            print(res)
 driver.close()
 
 try:
