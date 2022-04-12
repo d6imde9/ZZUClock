080ea633a823ffa8bf8ae86767866d70c40477eb
d6imde9 <50021127+d6imde9@users.noreply.github.com>
2022-04-12 10:07:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ for acc in account:
         else: #进入打卡界面，点击“提交表格”
             driver.find_element(by=By.XPATH, value='//*[@id="bak_0"]/div[11]/div[3]/div[4]').click()
             driver.find_element(by=By.XPATH, value='//*[@id="btn416b"]').click()
-            sleep(1)
+            sleep(5)
             res = driver.find_element(by=By.XPATH, value='//*[@id="bak_0"]/div[2]/div[2]/div[2]/div[2]').text
             if "同学，感谢你今日上报健康状况" not in res:
                 errors += 1
