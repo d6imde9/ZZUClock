b9ee9290c1b3c928f1c5c67ee17e65cf3c731e45
d6imde9 <50021127+d6imde9@users.noreply.github.com>
2022-04-14 09:40:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,73 +1,58 @@
-#coding:utf-8
-from time import sleep
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+# coding:utf-8
 import os
 
+import selenium
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+
+os.environ["webdriver.chrome.driver"] = '/usr/bin/chromedriver'
 option = webdriver.ChromeOptions()
-option.add_argument('--headless')
-option.add_argument('--no-sandbox')
-option.add_argument('--ignore-certificate-errors')
-option.add_argument("blink-settings=imagesEnabled=false")
-pref = {"profile.default_content_setting_values.geolocation" :2}
-option.add_experimental_option("prefs",pref)
-option.add_experimental_option('excludeSwitches', ['enable-automation'])
-os.environ["webdriver.chrome.driver"] = "/usr/bin/chromedriver"
-driver = webdriver.Chrome(options=option, executable_path="/usr/bin/chromedriver") #启动浏览器
+option.add_argument('--headless')  # 启用无头模式
+option.add_argument('--ignore-certificate-errors')  # 无视SSL错误
+option.add_argument("blink-settings=imagesEnabled=false")  # 禁用图片
+pref = {"profile.default_content_setting_values.geolocation": 2}
+option.add_experimental_option("prefs", pref)  # 禁用地理位置
+option.add_experimental_option('excludeSwitches', ['enable-automation'])  # 启用开发者模式
+serv = Service("/usr/bin/chromedriver")
+driver = webdriver.Chrome(options=option, service=serv)  # 启动浏览器
 
-account = os.environ.get('ACCOUNT').split(';') #字符串预处理
-errors = 0
-
+err = 0
+account = os.environ.get('ACCOUNT').split(';')  # 字符串预处理
 for acc in account:
-    usr = acc.split(',')
-    
-    driver.get('https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/first0') #进入登陆界面
+    try:
+        driver.get('https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/first0')  # 进入登陆界面
+    except selenium.common.exceptions.WebDriverException:
+        print("SSL错误")
+        err += 1
+        continue
     driver.implicitly_wait(1)
-    
-    driver.find_element(by=By.NAME, value='uid').send_keys(usr[0])
-    driver.find_element(by=By.NAME, value='upw').send_keys(usr[1])
-    driver.find_element(by=By.NAME, value='myform52').submit()
-    driver.implicitly_wait(1)
-    
-    if driver.current_url == 'https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/login':
-        print('登录失败')
-        errors += 1
-    else: 
-        frame = driver.find_element(by=By.NAME, value='zzj_top_6s') #进入信息确认界面
-        driver.switch_to.frame(frame)
-        
-        if driver.find_element(by=By.XPATH, value='//*[@id="bak_0"]/div[11]/div[1]/span[2]').text == usr[0]:
-            if driver.find_element(by=By.XPATH, value='//*[@id="bak_0"]/div[5]/span').text == "今日您已经填报过了":
-                print("已填报")
-            else: #进入打卡界面，点击“提交表格”
-                driver.find_element(by=By.XPATH, value='//*[@id="bak_0"]/div[11]/div[3]/div[4]').click()
-                driver.implicitly_wait(1)
-                
-                if driver.find_element(by=By.XPATH, value='//*[@id="bak_0"]/div[5]/span[1]').text == usr[0]:
-                    driver.find_element(by=By.XPATH, value='//*[@id="btn416a"]').click()
-                    driver.implicitly_wait(5)
-                    
-                    if driver.current_url == 'https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/jksb':
-                        res = driver.find_element(by=By.XPATH, value='/html/body/form/div[1]/div[2]/div[2]/div[2]/div[2]').text
-                        if "同学，感谢你今日上报健康状况" not in res:
-                            errors += 1
-                        print(res)
-                    else:
-                        print("打卡异常3")
-                        errors += 1
-                else:
-                    print("打卡异常2")
-                    errors += 1
-        else:
-            print("打卡异常1")
-            errors +=1
-    sleep(1)
-driver.close()
 
-try:
-    if errors > 0:
-        raise Exception("打卡失败")
-except Exception:
-    print(errors)
+    driver.find_element(by=By.NAME, value='uid').send_keys("201884160115")
+    driver.find_element(by=By.NAME, value='upw').send_keys("09292517")
+    driver.find_element(by=By.NAME, value='myform52').submit()  #TODO:连续登录可能会出现验证码
+    driver.implicitly_wait(1)
+
+    if driver.current_url == 'https://jksb.v.zzu.edu.cn/vls6sss/zzujksb.dll/login':  # 登录错误
+        res = driver.find_element(by=By.XPATH, value='//*[@id="bak_0"]/div[2]/div[2]/div[2]/div[2]').text
+        print(res)
+        err += 1
+    else:
+        iframe = driver.find_element(by=By.NAME, value='zzj_top_6s')  # 进入信息确认界面
+        driver.switch_to.frame(iframe)  # 切换为子页面
+        driver.find_element(by=By.XPATH, value='//*[@id="bak_0"]/div[11]/div[3]/div[4]').click()  # 进入打卡界面
+        driver.implicitly_wait(1)
+
+        driver.find_element(by=By.XPATH, value='//*[@id="btn416a"]').click()  # 点击提交
+        driver.implicitly_wait(1)
+
+        res = driver.find_element(by=By.XPATH, value='//*[@id="bak_0"]/div[2]/div[2]/div[2]/div[2]')
+        if "感谢你今日上报健康状况" not in res.text:
+            err += 1
+        print(res.text)
+driver.close()
+if err > 0:
+    print("打卡共", err, "个异常")
+    raise Exception
 else:
-    print("打卡完成")
+    print("打卡完成，无异常")
