1f4a2199ac04a9dc71481eb2d4b5dcdc682e23a1
d6imde9 <50021127+d6imde9@users.noreply.github.com>
2022-04-17 10:25:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -46,8 +46,7 @@ for acc in account:
             driver.find_element(by=By.XPATH, value='//*[@id="bak_0"]/div[11]/div[3]/div[4]').click()  # 进入打卡界面
             driver.implicitly_wait(1)
 
-            driver.find_element(by=By.NAME, value='btn416a').click()
-            driver.find_element(by=By.XPATH, value='//*[@id="btn416a"]').click()  # 点击提交
+            driver.find_element(by=By.ID, value='btn416a').click()  # 点击提交
             driver.implicitly_wait(1)
 
             res = driver.find_elements(by=By.XPATH, value='//*[@id="bak_0"]/div[2]/div[2]/div[2]/div[2]')
